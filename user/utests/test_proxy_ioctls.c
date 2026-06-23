e1141f71e1f88077b22f6aa7c8033313eb4c4c26
Liav A. <liavalb@gmail.com>
2026-06-23 22:57:42 +0300

    Add a utest to test eventfd ioctls
    I still need to test this on the Olimex board, but it compiles for now.
    
    It should check if we can register and unregister eventfds properly.
    Pure functionality of the eventfd's will be tested later on, after we
    actually use eventfd's as a wakeup mechanism for userspace when there
    are incoming packets for processing.

--- user/utests/test_proxy_ioctls.c
+++ user/utests/test_proxy_ioctls.c
@@ -1,10 +1,12 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
+#include <sys/eventfd.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 
 #include "proxy_dev.h"
 #include "proxy_ioctl.h"
@@ -75,6 +77,74 @@ static void test_proxy_get_stats_info_cmd(int fd)
         printf("  OK\n");
 }
 
+static void test_proxy_eventfd_register_unregister(int fd)
+{
+	int ret;
+
+	printf("[TEST] EVENT FD REGISTER IOCTL (expect -EINVAL)\n");
+
+	int read_efd = eventfd(0, 0);
+	if (read_efd < 0) {
+		printf("  FAIL: failed to allocate read eventfd: %s\n",
+		    strerror(errno));
+		return;
+	}
+
+	int write_efd = eventfd(0, 0);
+	if (write_efd < 0) {
+		printf("  FAIL: failed to allocate write eventfd: %s\n",
+		    strerror(errno));
+		return;
+	}
+
+	struct proxy_register_eventfd reg_efd;
+
+	reg_efd.type = PROXY_EVENTFD_WRITE_BUFFER;
+	reg_efd.fd = write_efd;
+
+	ret = ioctl(fd, PROXY_IOC_REGISTER_EVENTFD, &reg_efd);
+	if (ret < 0)
+		printf("  FAIL: PROXY_IOC_REGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_WRITE_BUFFER): %s\n",
+		    strerror(errno));
+	else
+		printf("  OK: PROXY_IOC_REGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_WRITE_BUFFER)\n");
+
+	reg_efd.type = PROXY_EVENTFD_READ_BUFFER;
+	reg_efd.fd = read_efd;
+
+	ret = ioctl(fd, PROXY_IOC_REGISTER_EVENTFD, &reg_efd);
+	if (ret < 0)
+		printf("  FAIL: PROXY_IOC_REGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_READ_BUFFER): %s\n",
+		    strerror(errno));
+	else
+		printf("  OK: PROXY_IOC_REGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_READ_BUFFER)\n");
+
+	struct proxy_unregister_eventfd unreg_efd;
+	unreg_efd.type = PROXY_EVENTFD_WRITE_BUFFER;
+	ret = ioctl(fd, PROXY_IOC_UNREGISTER_EVENTFD, &unreg_efd);
+	if (ret < 0)
+		printf("  FAIL: PROXY_IOC_UNREGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_WRITE_BUFFER): %s\n",
+		    strerror(errno));
+	else
+		printf("  OK: PROXY_IOC_UNREGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_WRITE_BUFFER)\n");
+
+	unreg_efd.type = PROXY_EVENTFD_READ_BUFFER;
+	ret = ioctl(fd, PROXY_IOC_UNREGISTER_EVENTFD, &unreg_efd);
+	if (ret < 0)
+		printf("  FAIL: PROXY_IOC_UNREGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_READ_BUFFER): %s\n",
+		    strerror(errno));
+	else
+		printf("  OK: PROXY_IOC_UNREGISTER_EVENTFD "
+		       "(PROXY_EVENTFD_READ_BUFFER)\n");
+}
+
 static void test_unknown_ioctl(int fd)
 {
     int ret;
@@ -106,6 +176,7 @@ int main(int argc, char **argv)
 	test_proxy_get_mtd_info_cmd(fd);
 	test_proxy_get_shm_info_cmd(fd);
 	test_proxy_get_stats_info_cmd(fd);
+	test_proxy_eventfd_register_unregister(fd);
 	test_unknown_ioctl(fd);
 
 	close(fd);
