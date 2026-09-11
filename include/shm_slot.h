e4fe7afb8df07abb573e93b1e743ef4b4a812392
Liav A. <liavalb@gmail.com>
2026-09-12 00:28:10 +0300

    Fix a slightly wrong phrase on datalen and ooblen in struct shm_slot_hdr

--- include/shm_slot.h
+++ include/shm_slot.h
@@ -86,7 +86,8 @@ struct shm_slot_hdr {
 	 */
 	struct nand_io_position_params pos_params;
 
-	/* These values have double-meaning, in accordance to their slot usage -
+	/* These values have are set with respect to the I/O request that
+	 * is occuring within the slot -
 	 *
 	 * For write slots, they represent the data and OOB buffer lengths that
 	 * are specified by the upper MTD write_oob callee.
